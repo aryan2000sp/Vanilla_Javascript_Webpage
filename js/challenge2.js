eea16a2f775676e894848bdbc8786e446d82b17b
Aryan <aryan2000sp@gmail.com>
2021-01-09 20:09:01 -0500

    Deleted Some useless files

--- js/challenge2.js
+++
@@ -1,9 +0,0 @@
-function addingImage()
-{
-	//First create a new image tag
-	var imageElement=document.createElement('img');
-
-	return imageElement;
-}
-
-console.log(document.getElementById('actionButton').onclick=addingImage());
